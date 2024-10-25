4f585e6b8a821150de052ca20ab30c07f28f6fdb
MegaInfernape20 <andersonadrielanthony@gmail.com>
2024-10-24 20:49:14 -0400

    camera and movement do things

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -11,23 +13,40 @@ public class Player : MonoBehaviour
     public IInteractable Interactable { get; set; }
 
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    public float speed = 0;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
     private void Update()
     {
         if (dialogueUI.isOpen) return;
 
-
-        // something somethin, use get.input somewhere in here to deal with movement, that's your part Adriel
-
-        
+      
         if (Input.GetKeyDown(KeyCode.E))
         {
             Interactable?.Interact(this);
         }
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+        Debug.Log(movement);
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 }
